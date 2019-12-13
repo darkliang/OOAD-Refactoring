bb720ebe5a0b523938b9836d1f883956b6a9abdb
Jonhao Leung <11710911@mail.sustech.edu,cn>
2019-12-13 12:05:17 +0800

    extract dateRange

--- src/Main.java
+++ src/Main.java
@@ -20,37 +20,37 @@ public class Main {
         C3 = new Customer("Manny");
 
         M1_Title = "Oz The Great and Powerful";
-        M1_PriceCode = Rental.NEW_RELEASE;
+        M1_PriceCode = Movie.NEW_RELEASE;
 
         M2_Title = "The Dark Knight";
-        M2_PriceCode = Rental.REGULAR;
+        M2_PriceCode = Movie.REGULAR;
 
         M3_Title = "Wreck-it Ralph";
-        M3_PriceCode = Rental.CHILDRENS;
+        M3_PriceCode = Movie.CHILDRENS;
 
         Date start = new Date(2013, 7, 1);
         Date end1 = new Date(2013, 7, 6);
         Date end2 = new Date(2013, 7, 4);
         Date end3 = new Date(2013, 7, 5);
-        C1.addRental(new Rental(M1_Title, M1_PriceCode, start, end1));
-        C1.addRental(new Rental(M2_Title, M2_PriceCode, start, end2));
-        C1.addRental(new Rental(M3_Title, M3_PriceCode, start, end3));
+        C1.addRental(new Rental(null, new DateRange(start, end1)));
+        C1.addRental(new Rental(null, new DateRange(start, end2)));
+        C1.addRental(new Rental(null, new DateRange(start, end3)));
         System.out.println(C1.statement()); 
         System.out.println();
 
         start = new Date(2013, 7, 11);
         end1 = new Date(2013, 7, 12);
         end2 = new Date(2013, 7, 19);
-        C2.addRental(new Rental(M1_Title, M1_PriceCode, start, end1));
-        C2.addRental(new Rental(M3_Title, M3_PriceCode, start, end2));
+        C2.addRental(new Rental(null, new DateRange(start, end1)));
+        C2.addRental(new Rental(null, new DateRange(start, end2)));
         System.out.println(C2.statement()); 
         System.out.println();
 
         start = new Date(2013, 6, 1);
         end1 = new Date(2013, 6, 3);
         end2 = new Date(2013, 6, 2);
-        C3.addRental(new Rental(M2_Title, M2_PriceCode, start, end1));
-        C3.addRental(new Rental(M3_Title, M3_PriceCode, start, end2));
+        C3.addRental(new Rental(null, new DateRange(start, end1)));
+        C3.addRental(new Rental(null, new DateRange(start, end2)));
         System.out.println(C3.statement()); 
         System.out.println();
     }
